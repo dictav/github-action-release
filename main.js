598e5aaf6f89bfd88d0c98027eb5f59af5c560b7
Shintaro Abe <dictav@gmail.com>
2020-03-30 10:39:55 +0900

    add flow for static analysis of code

--- main.js
+++ main.js
@@ -6,6 +6,12 @@ import { context } from '@actions/github';
 try {
   // const token = core.getInput('github-token');
   // const client = new github.GitHub(token);
+
+  for (const key in context) {
+    const val = context[key];
+    info(`${key}: ${JSON.stringify(val)}`);
+  }
+
   const payload = context.payload;
 
   info(payload.title);
